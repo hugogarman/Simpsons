9a1cde6db6e7e1f5b5fd4785f440f50c1e144c13
hugogarman <hugo.garman.2@educa.jcyl.es>
2025-10-31 17:10:43 +0100

    Add.github

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.simpsons"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.simpsons"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
